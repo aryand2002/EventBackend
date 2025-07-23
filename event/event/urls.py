862eeb4cb690de17d7b644f508b22549274a0166
aryand7265@gmail.com <aryand7265@gmail.com>
2025-07-23 17:52:15 +0530

    Initial commit

---
+++ event/event/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for event project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myevent.views import *
+from rest_framework_simplejwt.views import TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+     path('decorations/', DecorationListCreateAPIView.as_view(), name='decoration-list-create'),
+    path('decorations/<int:pk>/', DecorationDetailAPIView.as_view(), name='decoration-detail'),
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ event/myevent/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyeventConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myevent'
